ba3c44c18eaa572ab17209c0a4b4490962dd5f30
josipas <josipa2supe@gmail.com>
2022-03-20 20:23:15 +0100

    Added bottom imageViews

--- sofascore-academy/FirstVC.swift
+++ sofascore-academy/FirstVC.swift
@@ -12,6 +12,8 @@ class FirstVC: UIViewController {
     let centralImageView = UIImageView()
     let topLeadingImageView = UIImageView()
     let topTrailingImageView = UIImageView()
+    let bottomLeadingImageView = UIImageView()
+    let bottomTrailingImageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,6 +55,33 @@ class FirstVC: UIViewController {
             topTrailingImageView.heightAnchor.constraint(equalToConstant: 150),
             topTrailingImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             topTrailingImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
+
+
+        guard let tabBarHeight = self.tabBarController?.tabBar.frame.height else { return }
+
+
+        view.addSubview(bottomLeadingImageView)
+        bottomLeadingImageView.translatesAutoresizingMaskIntoConstraints = false
+        bottomLeadingImageView.image = .checkmark
+
+        NSLayoutConstraint.activate([
+            bottomLeadingImageView.widthAnchor.constraint(equalToConstant: 150),
+            bottomLeadingImageView.heightAnchor.constraint(equalToConstant: 150),
+            bottomLeadingImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -tabBarHeight - 10),
+            bottomLeadingImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
+        ])
+
+
+        view.addSubview(bottomTrailingImageView)
+        bottomTrailingImageView.translatesAutoresizingMaskIntoConstraints = false
+        bottomTrailingImageView.image = .checkmark
+
+        NSLayoutConstraint.activate([
+            bottomTrailingImageView.widthAnchor.constraint(equalToConstant: 150),
+            bottomTrailingImageView.heightAnchor.constraint(equalToConstant: 150),
+            bottomTrailingImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -tabBarHeight - 10),
+            bottomTrailingImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10)
         ])
     }
 }
