6696008a3ed4e3499ed558a297da67487abcce4b
josipas <josipa2supe@gmail.com>
2022-03-29 22:07:27 +0200

    Added button and text field in TransportVC

--- sofascore-academy/ViewControllers/TransportDetailsVC.swift
+++ sofascore-academy/ViewControllers/TransportDetailsVC.swift
@@ -3,6 +3,9 @@ import UIKit
 class TransportDetailsVC: UIViewController {
     private var collectionView: UICollectionView!
     private var titleLabel: UILabel!
+    private var detailsButton: CustomButton!
+    private var detailsText: UITextView!
+
     private let cellIdentifier = "cellId"
 
     private var transport: Transport?
@@ -42,19 +45,26 @@ class TransportDetailsVC: UIViewController {
                                                 x: 0,
                                                 y: 0,
                                                 width: (view.bounds.width),
-                                                height: (view.bounds.height/2)),
+                                                height: 350),
                                           collectionViewLayout: flowLayout)
-
-        titleLabel = UILabel()
-
         collectionView.register(CustomCell.self, forCellWithReuseIdentifier: cellIdentifier)
         collectionView.delegate = self
         collectionView.dataSource = self
+
+        titleLabel = UILabel()
+
+        detailsButton = CustomButton(title: "More", color: .systemPink)
+        detailsButton.delegate = self
+
+        detailsText = UITextView()
+        detailsText.isHidden = true
     }
 
     private func addSubviews() {
         view.addSubview(titleLabel)
         view.addSubview(collectionView)
+        view.addSubview(detailsButton)
+        view.addSubview(detailsText)
     }
 
     private func styleViews() {
@@ -62,6 +72,13 @@ class TransportDetailsVC: UIViewController {
 
         titleLabel.text = transport.description
         titleLabel.textColor = .systemPink
+
+        detailsText.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        detailsText.backgroundColor = .systemGray
+        detailsText.textColor = .white
+        detailsText.font = .systemFont(ofSize: 14)
+        detailsText.layer.cornerRadius = 8
+        detailsText.clipsToBounds = true
     }
 
     private func addConstraints() {
@@ -69,6 +86,19 @@ class TransportDetailsVC: UIViewController {
             make.centerX.equalToSuperview()
             make.height.equalTo(50)
             make.top.equalTo(collectionView.snp.bottom)
+        }
+
+        detailsButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(40)
+            $0.trailing.equalToSuperview().offset(-40)
+            $0.top.equalTo(titleLabel.snp.bottom)
+        }
+
+        detailsText.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(40)
+            $0.trailing.equalToSuperview().offset(-40)
+            $0.height.equalTo(200)
+            $0.top.equalTo(detailsButton.snp.bottom).offset(5)
         }
     }
 }
@@ -94,5 +124,18 @@ extension TransportDetailsVC: UICollectionViewDelegate {
 extension TransportDetailsVC: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 100, height: 100)
+    }
+}
+
+extension TransportDetailsVC: CustomButtonDelegate {
+    func didTapCustomButton() {
+        switch detailsText.isHidden {
+        case true:
+            detailsText.isHidden = false
+            detailsButton.setTitle("Less", for: .normal)
+        case false:
+            detailsText.isHidden = true
+            detailsButton.setTitle("More", for: .normal)
+        }
     }
 }
